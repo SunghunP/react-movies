a36662457d9689894703cc5b1be9c830d2f1915e
Sunghun Park <sunghun.josephp@gmail.com>
2022-05-07 11:28:39 -0700

    Add movie prop to MovieDisplay component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Form from './components/Form'
 import MovieDisplay from './components/MovieDisplay'
+import { useState } from 'react';
 
 function App() {
+
+  const [movie, setMovie] = useState(null)
+
   return (
     <div className="App">
       <Form />
-      <MovieDisplay />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
