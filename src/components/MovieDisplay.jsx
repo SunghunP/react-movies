6e8d9ee5037f8e55b72155d3072d2301e6f39048
Sunghun Park <sunghun.josephp@gmail.com>
2022-05-07 14:41:04 -0700

    Add comments

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,5 +1,25 @@
-const MovieDisplay = ({movie, Title, Poster, Writer, Genre, Released, Plot}) => {
+// // destructure the movie prop
+// const MovieDisplay = ({ movie }) => {
+//   if (movie) {
+//     // destructuring properties of movie
+//     const { Title, Poster, Writer } = movie;
+//     return (
+//       <div>
+//         <h1>{Title}</h1>
+//         <img src={Poster} alt={Title} />
+//         <h3>{Writer}</h3>
+//       </div>
+//     );
+//   } else {
+//     return <></>
+//   }
+// };
+
+// export default MovieDisplay;
+
+const MovieDisplay = ({movie}) => {
 	return !!movie && <div>
+		
 		<h1>{movie.Title}</h1>
 		<img src={movie.Poster} alt={movie.Title}></img>
 		<h3>{movie.Writer}</h3>
