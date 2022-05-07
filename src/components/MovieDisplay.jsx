740005cd40f3e2ddbed40f5ba41c0cba52f21d0a
Sunghun Park <sunghun.josephp@gmail.com>
2022-05-07 14:26:10 -0700

    Use object destructuring

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,6 +1,11 @@
-const MovieDisplay = (props) => {
-	return props.movie && <div>
-		<h1>{props.movie.Title}</h1>
+const MovieDisplay = ({movie, Title, Poster, Writer, Genre, Released, Plot}) => {
+	return !!movie && <div>
+		<h1>{movie.Title}</h1>
+		<img src={movie.Poster} alt={movie.Title}></img>
+		<h3>{movie.Writer}</h3>
+		<h3>{movie.Genre}</h3>
+		<h3>{movie.Released}</h3>
+		<p>{movie.Plot}</p>
 	</div>
 }
 
