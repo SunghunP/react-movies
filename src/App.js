b0d168b87670de168db579da6c2463c047cd0e16
Sunghun Park <sunghun.josephp@gmail.com>
2022-05-07 10:53:31 -0700

    Add initial skeleton of components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Form from './components/Form'
+import MovieDisplay from './components/MovieDisplay'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+//http://www.omdbapi.com/?apikey=8e2fa05a&t=godfather
+
+export default App;
---
+++ src/components/MovieDisplay.jsx
@@ -0,0 +1,5 @@
+const MovieDisplay = (props) => {
+	return <h1>Form</h1>
+}
+
+export default MovieDisplay
